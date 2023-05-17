1baa410670b5b8ec093a18310024a07207a592af
sawonhossain34 <sawonhossain3491@gmail.com>
2023-05-17 13:09:23 +0600

    login and sign up page ,firebase i , create sign in onauthStatechange set done

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './routes/routes.jsx';
+import AuthProviders from './provider/AuthProviders';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='max-w-7xl mx-auto'>
     <React.StrictMode>
+      <AuthProviders>
       <RouterProvider router={router} />
+      </AuthProviders>
     </React.StrictMode>,
   </div>
 )
---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,71 @@
+import { Link } from "react-router-dom";
+import login from '../../assets/image/about/login-img.jpg'
+import { useContext } from "react";
+import { AuthContext } from "../../provider/AuthProviders";
+
+const SignUp = () => {
+
+    const {createUser} = useContext(AuthContext);
+
+    const handleSignUp = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name,email,password);
+
+        createUser(email,password)
+        .then(result => {
+            const loggedUser = result.user;
+            console.log(loggedUser);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+
+    }
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="text-center lg:text-left  mr-12  w-1/2">
+                    <img className='' src={login} alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <h1 className="text-3xl font-bold text-center">Sign Up</h1>
+                        <form onSubmit={handleSignUp}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="name" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input className="btn btn-primary" type="submit" value="Sign Up" />
+                            </div>
+                        </form>
+                        <p className='py-4 text-center'>Already have an account <Link className='text-yellow-500 font-bold' to="/login">Login</Link></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
